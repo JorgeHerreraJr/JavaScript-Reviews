bc6480e5e67260a5a885c4fbc4efcd7dea3e6e54
JorgeHerreraJr <herreraljorge64@gmail.com>
2021-09-07 18:49:31 -0400

    Created app.js.

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const reviews = [{
+    id: 1,
+    name: "alora pratt",
+    job: "chemist",
+    img: "",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur quae totam expedita quia vero recusandae quos corrupti dolor praesentium a.",
+},
+{
+    id: 2,
+    name: "jane oakley, quinn ripley, scarlet ryder, willow timber",
+    job: "doctor, paralegal, electrician, cost analyst",
+    img: "",
+    text: "Lorem ipsum dolor sit",
+},
+{
+    id: 3,
+    name: "quinn ripley",
+    job: "paralegal",
+    img: "",
+    text: "Lorem ipsum dolor sit, amet",
+},
+{
+    id: 4,
+    name: "scarlet ryder",
+    job: "electrician",
+    img: "",
+    text: ""
+}]
